d20811e0c7e3d497ffdad5e4dce4034e24a35e28
MartryatusSofia <92001593+MartryatusSofia@users.noreply.github.com>
2022-11-04 13:21:14 +0700

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,62 +19,63 @@ class AuthController extends Controller
     }
 
     public function register(Request $request)
-{
-    $fields = $request->validate([
-        'name' => 'required|string|max:100',
-        'email' => 'required|string|unique:users,email',
-        'password' => 'required|string|confirmed|min:6'
-    ]);
+    {
+        $fields = $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed|min:6'
+        ]);
 
-    $user = User::create([
-        'name' => $fields['name'],
-        'email' => $fields['email'],
-        'password' => bcrypt($fields['password'])
-    ]);
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
 
-    $token = $user->createToken('tokenku')->plainTextToken;
+        $token = $user->createToken('tokenku')->plainTextToken;
 
-    $response = [
-        'user' => $user,
-        'token' => $token
-    ];
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
 
-    return response($response, 201);
-}
+        return response($response, 201);
+    }
 
-public function login(Request $request)
-{
-    $fields = $request->validate([
-        'email' => 'required|string',
-        'password' => 'required|string'
-    ]);
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
 
-    //chek email
-    $user = User::where('email', $fields['email'])->first();
+        // check email
+        $user = User::where('email', $fields['email'])->first();
 
-    //chek password
-    if (!$user || !Hash::check($fields['password'], $user->password)){
+        // check password
+        if (!$user || !Hash::check($fields['password'], $user->password)){
         return response([
             'message' => 'unauthorized'
         ], 401);
-    }
+        }
+
+        $token = $user->createToken('tokenku')->plainTextToken;
     
-    $token = $user->createToken('tokenku')->plainTextToken;
-
-    $response = [
-        'user' => $user,
-        'token' => $token
-    ];
-
-    return response($response, 201);
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+    
+        return response($response, 201);
+        
     }
-
     public function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
 
-         return[
-             'message' => 'Logged out'
+        return [
+            'message' => 'Logged out'
         ];
     }
-}
+
+};
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,36 +20,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//public route
+// public route
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/books', [BookController::class, 'index']);
-Route::get('/Books/{id}', [BookController::class, 'show']);
-Route::get('/Authors', [BookController::class, 'index']);
-Route::get('/Authors/{id}', [BookController::class, 'show']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::get('/authors/{id)', [AuthorController::class, 'show']);
 
-//protected routes
-Route::middleware('auth:sanctum')->group(function(){
-    Route::resource('books', BookController::class)->except('create','edit','show','index');
-    Route::post('/logout',[AuthController::class,'logout']);
-    Route::resource('authors', AuthController::class)->except('create','edit','show','index');
+// protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('authors', AuthorController::class)->except('create', 'edit', 'show', 'index');
 });
-
-//Route::get('/me', [AuthController::class, 'me']);
-
-//Route::get('/books', [BookController::class, 'index']);
-//Route::get('/books/{id}', [BookController::class, 'show']);
-//Route::get('/books', [BookController::class, 'store']);
-//Route::get('/books/{id}', [BookController::class, 'update']);
-//Route::get('/books/{id}', [BookController::class, 'destroy']);
-
-//Route::resource('books', BookController::class)->except(
-//    ['create','edit']
-//);
-
-//Route::resource('authors', AuthorController::class)->except(
-//  ['create','edit']
-//);
-
-
